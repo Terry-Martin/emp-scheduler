231356ca0eccb74b502dcebae43f171826d4fea6
Terry-Martin <>
2023-06-15 20:41:16 +0000

    Add emp_details model

--- emp_scheduler_app/admin.py
+++ emp_scheduler_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Emp_Details
 
+admin.site.register(Emp_Details)
 # Register your models here.
--- emp_scheduler_app/models.py
+++ emp_scheduler_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Emp_Details(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
